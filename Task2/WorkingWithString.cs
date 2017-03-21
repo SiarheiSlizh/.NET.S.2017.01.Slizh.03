be84c6a29248d73e8dfc6087b956c514926cf31f
сергей слиж <sergeislizh@mail.ru>
2017-03-21 22:01:37 +0300

    modified

--- Task2/WorkingWithString.cs
+++ Task2/WorkingWithString.cs
@@ -12,49 +12,42 @@ namespace Task2
     public static class WorkingWithString
     {
         #region concatenating strings
+
         /// <summary>
         /// This method concatinates two strings Without repetition of symbols.
         /// </summary>
         /// <param name="firstString">This argument accepts the reference which point to first string</param>
         /// <param name="secondString">This argument accepts the reference which point to second string</param>
-        /// <returns>This method returns the concatination of two strings</returns>
+        /// <returns>The concatination of two strings</returns>
         public static string StringConcat(string firstString, string secondString)
         {
             if (Equals(firstString, null) || Equals(secondString, null)) 
                 throw new ArgumentNullException();
 
-            CheckStrings(firstString, secondString);
+            if (!firstString.IsAmericanSymbolsToLower() || !secondString.IsAmericanSymbolsToLower())
+                throw new ArgumentException();
 
             char[] str = string.Concat(firstString, secondString).Distinct().ToArray();
             Array.Sort(str);
             return new string(str);
         }
+
         /// <summary>
         /// This method checks strings for validity
         /// </summary>
-        /// <param name="firstString">This argument accepts the reference which point to first string</param>
-        /// <param name="secondString">This argument accepts the reference which point to second string</param>
-        static void CheckStrings(string firstString, string secondString)
+        /// <param name="str">String in which is checked wether it contains american symbols.</param>
+        /// <returns>True if string contains symbols in range of a-z else false.</returns>
+        static bool IsAmericanSymbolsToLower(this string str)
         {
             int i = 0;
-            while (i < firstString.Length && i < secondString.Length)
+           
+            while (i < str.Length)
             {
-                if (firstString[i] < 97 || firstString[i] > 122 || secondString[i] < 97 || secondString[i] > 122)
-                    throw new ArgumentException("Strings have to consists of symbols a-z");
+                if (str[i] < 97 || str[i] > 122)
+                    return false;
                 i++;
             }
-            while(i < firstString.Length)
-            {
-                if (firstString[i] < 97 || firstString[i] > 122)
-                    throw new ArgumentException("Strings have to consists of symbols a-z");
-                i++;
-            }
-            while (i < secondString.Length)
-            {
-                if (secondString[i] < 97 || secondString[i] > 122)
-                    throw new ArgumentException("Strings have to consists of symbols a-z");
-                i++;
-            }
+            return true;
         }
         #endregion
     }
